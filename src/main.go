8c4218f65d8bd1906d73ed37679a7afd23a554dd
Mackenzie Higa <mhiga@sfu.ca>
2019-04-04 19:27:45 -0700

    can load board an color

--- src/main.go
+++ src/main.go
@@ -7,8 +7,12 @@ import (
 )
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
+	screenWidth      = 600
+	screenHeight     = 600
+	boxSize          = 10
+	totalScreen      = screenWidth * screenHeight
+	screenDimensions = totalScreen / boxSize
+	rectSize         = screenHeight / boxSize
 )
 
 func main() {
@@ -40,28 +44,85 @@ func main() {
 		return
 	}
 
+	rectArray := make([]sdl.Rect, screenDimensions)
+	xCoor := 0
+	yCoor := 0
+	for i := 0; i < screenDimensions; i++ {
+		rectArray[i] = sdl.Rect{int32(xCoor), int32(yCoor), boxSize, boxSize}
+		xCoor = xCoor + boxSize
+
+		// Basically moving left to right and then reset a row down
+		if xCoor >= screenWidth {
+			xCoor = 0
+			yCoor = yCoor + boxSize
+		}
+	}
+
 	// Destroys renderer at end of execution
 	defer renderer.Destroy()
 
-	for {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
+	// startUp := false
+	prevX := int32(0)
+	prevY := int32(0)
 
+	for {
+		//initialize gameboard
+		// if startUp == false {
+		// 	renderer.SetDrawColor(255, 255, 255, 255)
+		// 	renderer.Clear()
+
+		// 	for i := 0; i < len(rectArray); i++ {
+		// 		renderer.SetDrawColor(0, 0, 0, 255)
+		// 		renderer.DrawRect(&rectArray[i])
+		// 	}
+
+		// 	renderer.Present()
+		// 	fmt.Println("turned screen white")
+		// 	startUp = true
+		// }
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch val := event.(type) {
 			// For exiting window
 			case *sdl.QuitEvent:
 				return
 
-			// State is for clicking (0 off: 1 on), Need x and y coordinates
-			case *sdl.MouseMotionEvent:
-				fmt.Printf("Mouse at: %+v\n ", event)
-			}
+			case *sdl.KeyboardEvent:
 
+				// Don't need to press this anymore but just if I want to use the keydown thing
+				if val.Keysym.Sym == sdl.K_ESCAPE {
+					fmt.Println("ESC pressed")
+
+					for i := 0; i < len(rectArray); i++ {
+						renderer.SetDrawColor(0, 0, 0, 255)
+						renderer.DrawRect(&rectArray[i])
+					}
+				}
+			}
 		}
 
-		renderer.SetDrawColor(255, 0, 0, 255)
-		renderer.Clear()
+		//checks if mouse is clicked
+		mouseX, mouseY, mouseButtonState := sdl.GetMouseState()
+
+		if mouseX != prevX && mouseY != prevY && mouseButtonState == 1 {
+			fmt.Printf("Mouse at x: %+v, y: %+v, state: %+v\n", mouseX, mouseY, mouseButtonState)
+			prevX = mouseX
+			prevY = mouseY
+
+			index := int((mouseX / boxSize) + rectSize*(mouseY/boxSize))
+
+			if index >= (rectSize)*(rectSize) {
+				fmt.Println("out of index:")
+				return
+			}
+
+			renderer.SetDrawColor(0, 0, 255, 255)
+			renderer.FillRect(&rectArray[index])
+			// renderer.Present()
+		}
 
 		renderer.Present()
+		renderer.SetDrawColor(255, 255, 255, 255)
+		renderer.Clear()
 
 	}
 
